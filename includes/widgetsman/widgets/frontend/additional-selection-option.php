d4b375151fc6a466daca2fa2991b0ab6a325f28a
Jorge Garrido <jegschl@gmail.com>
2024-08-18 22:28:49 -0400

    additional selection Templates

---
+++ includes/widgetsman/widgets/frontend/additional-selection-option.php
@@ -0,0 +1,17 @@
+<?php
+/* <?= $opt['id'] ?> : {{opt-id}}
+   <?= $opt['slug'] ?> : {{opt-slug}}
+   <?= $opt['label'] ?> : {{opt-label}}
+   <?= $opt['value'] ?> : {{opt-value}}
+   <?= $opt['value_type'] ?> : {{opt-value_type}}
+   <?= $opt['sub_options'] ?> : {{opt-sub_options}}
+   <?= $sopt['slug'] ?> : {{subopt-slug}}    
+   <?= $sopt['label'] ?> : {{subopt-label}}
+*/
+?>
+
+                        <div class="option-buton outer">
+                            <div class="option-buton" data-option="{{subopt-slug}}">{{subopt-label}}</div>
+                        </div>
+                    
+                
---
+++ includes/widgetsman/widgets/frontend/additional-selection-wrapper.php
@@ -0,0 +1,32 @@
+<?php
+
+
+?>
+
+<table 
+    class="variations field" 
+    data-jgbsbsc-field-type="additional-selection" 
+    cellspacing="0" 
+    data-field-addtiopnal-selection-id="{{additional-selection-id}}"
+>
+    <tbody>
+        <tr>
+        <td colspan="2" class="label">{{additional-selection-label}} </td>
+        </tr>
+        
+        <tr>
+            <td class="value" data-reg-val-id="{{opt-id}}">
+                <div class="wrapper">
+                    <label for="id-value-{{opt-slug}}">{{opt-label}}</label>
+                    <input type="radio" name="{{opt-slug}}" value="{{opt-value}}" id="id-value-{{opt-slug}}"/>
+                    
+                    <div class="buton-group {{opt-value_type}}" data-opts-sels="">
+
+                    {{#additional-selection-options}}
+
+                </div>
+            </td>
+        </tr>
+
+    </tbody>
+</table>
